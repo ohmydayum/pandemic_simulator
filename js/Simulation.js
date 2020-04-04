ed3b8bce7b1d72d5ae530b1d5309ea3a570adb9d
dor.israeli <dor.israeli@gmail.com>
2020-04-04 20:01:36 +0300

    3.3.7

--- js/Simulation.js
+++ js/Simulation.js
@@ -1,7 +1,7 @@
 class Simulation {
-    constructor(RESULTION, GRAPH_DURATION, CANVAS_WIDTH, CANVAS_HEIGHT, SKIPS, PAUSED) {
+    constructor(SHOW, RESULTION, CANVAS_WIDTH, CANVAS_HEIGHT, SKIPS, PAUSED) {
+        this.SHOW = SHOW;
         this.RESULTION = RESULTION;
-        this.GRAPH_DURATION = GRAPH_DURATION;
         this.CANVAS_WIDTH = CANVAS_WIDTH;
         this.CANVAS_HEIGHT = CANVAS_HEIGHT;
         this.SKIPS = SKIPS;
